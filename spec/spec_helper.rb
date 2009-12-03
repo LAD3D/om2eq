6e853d922a8f549c96d86324227a23cc77984278
Sergio Arbeo Rodríguez <sergio@Veronica.local>
2009-12-03 20:56:35 +0100

    Implemented some shared examples at spec_helper.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,3 +7,20 @@ require 'spec/autorun'
 Spec::Runner.configure do |config|
   
 end
+
+shared_examples_for "PointOrderedSet" do
+  
+  it "should fail if key is out of bounds" do
+    lambda{ @set[10000]="anything"}.should raise_error KeyOutOfBoundsError
+    lambda{ @set[-1]="anything"}.should raise_error KeyOutOfBoundsError
+  end
+  
+  it "should fail if value is not a Point" do
+    lambda{ @set[0]="anything"}.should raise_error ParamIsNotPointError
+  end
+  
+  it "should not raise an error if value is a Point" do
+    lambda{ @set[0]=Point.new}.should_not raise_error
+    lambda{ @set[0]=Class.new(Point).new}.should_not raise_error
+  end
+end
