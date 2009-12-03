cab75fd75a2567320f73285c4837468efca3686a
Sergio Arbeo Rodríguez <sergio@Veronica.local>
2009-12-04 00:04:59 +0100

    Some really cool metaprogramming features.

--- lib/om2ec/point_ordered_set.rb
+++ lib/om2ec/point_ordered_set.rb
@@ -1,9 +1,19 @@
 module PointOrderedSet
   
   def []=(key, val)
-    raise KeyOutOfBoundsError if key < 0 || key >= self.class.instance_variable_get("@limit")
+    raise KeyOutOfBoundsError if key < 0 || key >= self.class.limit
     raise ParamIsNotPointError unless val.is_a? Point
     @set[key] = val
+  end
+  
+  def self.included(base)
+    base.extend(ClassMethods)
+  end
+  
+  module ClassMethods
+    def limit
+      @limit
+    end
   end
 end
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,7 +12,7 @@ shared_examples_for "PointOrderedSet" do
   
   it "should fail if key is out of bounds" do
     lambda{ @set[10000]="anything"}.should raise_error KeyOutOfBoundsError
-    lambda{ @set[@set.class.instance_variable_get("@limit")]="anything"}.should raise_error KeyOutOfBoundsError
+    lambda{ @set[@set.class.limit]="anything"}.should raise_error KeyOutOfBoundsError
     lambda{ @set[-1]="anything"}.should raise_error KeyOutOfBoundsError
   end
   
