9d31cac0cc78288181b3b97465cc7086b1ffdf22
Sergio <sergio@wild.mat.ucm.es>
2010-01-13 14:46:57 +0100

    Added an spec for NumberGenerator

--- spec/om2ec/helpers/number_generator_spec.rb
+++ spec/om2ec/helpers/number_generator_spec.rb
@@ -8,5 +8,10 @@ describe NumberGenerator do
   it "should be a singleton class" do
     NumberGenerator.instance.should be_equal(@number_generator)
   end
+
+  it "should call instance method when class method is called" do
+    @number_generator.should_receive(:succ)
+    NumberGenerator.succ
+  end
 end
 
