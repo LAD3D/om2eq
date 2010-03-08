a9a60370e8bdc430e97e4da9cf05b30e10630825
Sergio Arbeo <serabe@gmail.com>
2010-03-08 17:45:19 +0100

    Added correct x, y and z methods to Midpoint.

--- lib/om2ec/constructions/midpoint.rb
+++ lib/om2ec/constructions/midpoint.rb
@@ -3,4 +3,16 @@ class Midpoint < SymbolicPoint
   def initialize(*args)
     @first_point, @second_point = retrieve_arguments(args)
   end
+
+  def x
+    @x ||= "((#{@first_point.x}-#{@second_point.x})/2)"
+  end
+
+  def y
+    @y ||= "((#{@first_point.y}-#{@second_point.y})/2)"
+  end
+
+  def z
+    @z ||= "((#{@first_point.z}-#{@second_point.z})/2)"
+  end
 end
